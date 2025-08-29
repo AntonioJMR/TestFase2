d868bc5de4fa156dbfa98eda887458f2e654f3f3
AntonioJMR <50738716+AntonioJMR@users.noreply.github.com>
2025-08-29 12:35:17 +0200

    Initial Swagger [New]

--- TestFase2/app/Http/Middleware/RoleMiddleware.php
+++ TestFase2/app/Http/Middleware/RoleMiddleware.php
@@ -6,12 +6,41 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+
+/**
+ * @OA\Info(
+ * title="Roles Middleware API",
+ * version="1.0.0",
+ * description="API para la gestión de roles de usuario",
+ * )
+ */
 class RoleMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * 
+    */
+
+    /**
+     * @OA\Get(
+     *     path="/api/admin/dashboard",
+     *     summary="Obtener el dashboard del administrador",
+     *     description="Requiere que el usuario esté autenticado y tenga el rol de 'admin'.",
+     *     security={{"bearerAuth": {}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Operación exitosa",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Bienvenido, administrador.")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Acceso denegado. El usuario no tiene el rol 'admin'."
+     *     )
+     * )
      */
     public function handle($request, Closure $next, $role) {
 
